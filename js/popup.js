b3b9e2a31d468853b3d2d7629d6799377345b10f
MuTTonye <murielle.akoo@live.fr>
2018-12-20 11:33:18 +0100

    Ajout du choix dans la popup et redirection vers page youtube

--- js/popup.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('html').addEvenListener('click', function popup () {
-	let message = alert("Nous vous proposons LE contrat de votre vie \nPour cela vous devez nous verser 250 milliards de $$$$ zimbabwéen !\nConnecte toi sur notre site www.xxxxx.com !");
-};)
-
-setTimeout(popup, 10000;);
